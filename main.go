265e34f4e0133c3c46fb64b59720b642d5a1f0ce
geegatomar <64457308+geegatomar@users.noreply.github.com>
2021-12-27 12:01:56 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
 func main() {
-	// Check the main code for stress testing with multiple goroutines in main_stress_test
+	// Check the main code for stress testing with multiple goroutines in main_stress_test.go file which shows
+	// the full usage of the cache with multiple goroutines trying to simultaneously access the cache,
+	// and the ability to handle that since its thread-safe
 }
